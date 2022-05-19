72d7b677293d2d4d813fe95113bf03eca28123b1
Varunsingh5 <96859377+Varunsingh5@users.noreply.github.com>
2022-05-19 11:05:05 +0530

    Update App.js
    useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
